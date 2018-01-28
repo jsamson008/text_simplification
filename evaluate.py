5758c473500cbb42c4f4cb930d5e6532421e931d
Devanshu Jain <5813054+jndevanshu@users.noreply.github.com>
2018-01-28 16:44:35 -0500

    Adds script to evaluate un-clustered file

---
+++ evaluate.py
@@ -0,0 +1,19 @@
+import json
+
+with open('rerank.json') as f:
+    data = json.load(f)
+
+bleu = 0
+sari = 0
+cnt = 0
+i = 0
+for item in data:
+    print(str(i) + " / " + str(len(data)))
+    i += 1
+    for cand in item['candidates'][:1]:
+        bleu += cand['bleu']
+        sari += cand['sari']
+        cnt += 1
+
+print(float(bleu) / float(cnt))
+print(float(sari) / float(cnt))
